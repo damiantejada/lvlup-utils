a2b7ed6993e5bb7ca0d7bdcfb34481aadb6e43a6
Damian Tejada <damiiiantejada@gmail.com>
2025-03-30 18:45:49 -0300

    chore: crear nueva de feat/utilities y agregar archivos iniciales

---
+++ src/arrays/map.ts
@@ -0,0 +1,13 @@
+// recibe un arary de items y una función de transformación (callback)
+// devuelve el array mapeado
+
+import { arraylable } from "./arraylable";
+
+// [2,4,6,8].map((num) => num *2)
+// ["hola","chau"].map((word) => word.length)
+
+export const map = <T, U>(array: T[], fn: (element: T) => U): U[] => {
+  // incluir un error de que no podemos mepaer algo que no es un array
+  if (!arraylable(array) || typeof fn !== "function") return [];
+  return map(array, fn);
+};
---
+++ src/timing/throttle.ts
@@ -0,0 +1,54 @@
+/*
+    Después de que la función se ejectua la primera vez se estable un intervalo de tiempo durante el cual no puede ejecutarse nuevamente incluso si se desencadenan más eventos
+*/
+
+/*
+
+    VISTA1 - BOTON(4) -VISTA2
+                      -VISTA2 
+                      -VISTA2 
+                      -VISTA2
+                      
+    VISTA1 - BOTON(THROTTLE) - VISTA2 
+*/
+
+// Esto nos tiene que devolver una función
+// fn('param1','param2) -> Parameters<T> (string,string)
+
+type AnyFunction = (...args: unknown[]) => unknown;
+
+// Esto nos tiene que devolver una función
+// Parameters<T>
+export function throttle<T extends AnyFunction>(
+  fn: T,
+  delay: number = 500
+): (...args: Parameters<T>) => void {
+  let timeOutId: ReturnType<typeof setTimeout> | undefined = undefined; // genera un timeOutId único para cada instancia de setTimeOut
+  return function thorttledFunction(...args: Parameters<T>) {
+    // La función está activa
+    // si la función ya se ejecutó y no pasó el tiempo, cortamos la ejecución
+    if (timeOutId !== undefined) {
+      return;
+    }
+    timeOutId = setTimeout(() => {
+      // cuando se termina el delay entra acá
+      timeOutId = undefined;
+    }, delay);
+    return fn(...args);
+  };
+}
+
+// setTimeout
+// return new fn manteniendo el scope
+
+/*
+funciton onClickButton(){
+    // some logic
+}
+
+const throttleClick = throttle(onClickButton, 500) // devuelve una función modificada con un "timer"
+
+<button onClick(throttleClick)>
+
+</button>
+*/
